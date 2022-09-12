629f0d47276718eefe381742e717024d441e803d
Yudho01011 <55352911+Yudho01011@users.noreply.github.com>
2022-09-12 12:43:57 +0700

    Update LogicTest.java
    modifikasi untuk function built in ke funtion sendiri

--- LogicTest.java
+++ LogicTest.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 // "static void main" must be defined in a public class.
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class Main {
         for (int i = 0; i < array.length; i++){
             String wordAnagram = array[i];
             for (int j = i; j < array.length-1; j++){
-                if (anagram(array[i], array[j+1])){
+		        if (anagram(array[i], array[j+1])){
                     wordAnagram += ","+array[j+1];
                     array[j+1] = " ";
                 }
@@ -17,23 +19,61 @@ public class Main {
                 result.add(temp);       
             }          
         }
-      
+        
         result.stream().forEach(elem -> System.out.println("element " + Arrays.toString(elem)));
      }
      
      public static boolean anagram(String root, String child){
          char [] wordOfRoot = root.toLowerCase().toCharArray();
          char [] wordOfChild = child.toLowerCase().toCharArray();
-         Arrays.sort(wordOfRoot);
-         Arrays.sort(wordOfChild);
+         wordOfRoot = sorting(wordOfRoot);
+         wordOfChild = sorting(wordOfChild);
          
-         return Arrays.equals(wordOfRoot, wordOfChild);
+         return compare(wordOfRoot, wordOfChild);
      }
     
     public static boolean checker(String [] x){
         return Arrays.asList(x)
                   .contains(" ");
     }
+
+    public static char[] sorting(char[] word){
+        
+        for(int i = 0; i<word.length; i++)   //Holds each element
+         {  
+             for (int j = i+1; j<word.length; j++)  //Check for remaining elements 
+             {  
+                //compares each elements of the array to all the remaining elements  
+                if( Character.compare(word[i],word[j])>0)   
+                {  
+                    //swapping array elements  
+                    char temp = word[i];  
+                    word[i] = word[j];  
+                    word[j] = temp;  
+                 }  
+              }  
+           }
+           
+           return word;
+    }
     
-    
+    public static boolean compare(char[] word1, char[] word2){
+	int counter = 0;
+	
+	if (word1.length != word2.length){
+	    return false;
+	}
+	
+	for (int i = 0; i < word1.length; i++){
+		if(word1[i] != word2[i]){
+			counter++;
+		}
+	}
+
+	if(counter != 0){
+		return false;
+	}else{
+		return true;
+	}
+}
 }
